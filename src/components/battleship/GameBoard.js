95bd7378f6db492502177519ee7ae7a957a373e1
RavenM <Ravenmetals@gmail.com>
2024-01-10 20:47:11 -0600

    feat: added onClick driver for board

--- src/components/battleship/GameBoard.js
+++ src/components/battleship/GameBoard.js
@@ -1,10 +1,11 @@
 import {useState} from "react";
 
-export function GameBoard() {
+const GameBoard = ({ boardEnabled }) => {
     const [selectedSquare, setSelectedSquare] = useState([])
 
-    function handleSquareClick(index) {
+    const handleSquareClick = (index) => {
         setSelectedSquare(prevState => {
+            if(!boardEnabled) return;
             if (prevState.includes(index)) {
                 return prevState.filter(square => square !== index);
             } else {
@@ -16,9 +17,12 @@ export function GameBoard() {
     return (
         <div className="grid grid-cols-10 grid-rows-10 gap-0.5 min-w-[250px]">
             {Array(100).fill(0).map((x, index) =>
-                <div className={`w-10 h-10 hover:bg-amber-300 ${selectedSquare.includes(index)? "bg-amber-50": "bg-amber-100"}`}
-                   onClick={() => handleSquareClick(index)}>
+                <div className={`w-10 h-10 ${boardEnabled && "hover:bg-amber-300"} ${selectedSquare.includes(index)? "bg-amber-50": "bg-amber-100"}`}
+                   onClick={() => handleSquareClick(index)}
+                    key={`${index}-${index%10}`}>
             </div>)}
         </div>
     )
 }
+
+export default GameBoard;
